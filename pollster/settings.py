a2419d4ad553da93ad5daf3f5122446c09a2b41e
NellutlaTejaswini <128363247+NellutlaTejaswini@users.noreply.github.com>
2023-05-22 12:32:10 +0530

    Update settings.py

--- pollster/settings.py
+++ pollster/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ayl0uawxbrh5-@unj4ky7@=mf8h_m_-y6c46-#(!f^w42(vx-w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['onlinevoting01.azurewebsites.net']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
